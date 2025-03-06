9ae9b410ee4af790d751737cf83b5896d6b0611f
no1pain <fe.oleksandr.kazan@gmail.com>
2025-03-06 11:10:36 +0200

    Remove GitHub Pages configuration for Vercel deployment

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  const basePath =
-    process.env.NODE_ENV === "production" ? "/next-cv-website" : "";
-
   return (
     <Html lang="en">
-      <Head>
-        {/* Add explicit link to CSS file */}
-        <link rel="stylesheet" href={`${basePath}/_next/static/css/app.css`} />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
