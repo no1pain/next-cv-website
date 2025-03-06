eeaab36ad9e4e1981dd9c14f9c5ba8afe00bded2
no1pain <lisovyi.ihor92@gmail.com>
2025-03-06 15:33:50 +0200

    Update ProjectsData.ts

--- src/app/components/projects/ProjectsData.ts
+++ src/app/components/projects/ProjectsData.ts
@@ -68,8 +68,8 @@ export const projects: Project[] = [
     icon: "M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01",
     gradient: "from-amber-500 via-orange-500 to-red-500",
     link: "#",
-    demoLink: "#",
-    githubLink: "#",
+    demoLink: "https://no1pain.github.io/Taskease_Manager/",
+    githubLink: "https://github.com/no1pain/Taskease_Manager",
   },
   {
     id: 4,
